2f1207428d71fd91a80014284a65e66809fb5313
Dhairyash Gupta <101561153+Dhairyash-1@users.noreply.github.com>
2023-03-20 16:38:12 +0530

    finished

--- Movie-App/main.js
+++ Movie-App/main.js
@@ -1,22 +1,50 @@
-const options = {
-  method: "GET",
+const APIURL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=5`;
+const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=`;
 
-  headers: {
-    "X-RapidAPI-Key": "ed6adc3187msh9c479bdd8dcd9f2p18d3c7jsn8e62560cc0d6",
-    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
-  },
-};
-const image = document.getElementById("image");
-async function fetchMovie(name) {
-  const res = await fetch(
-    `https://streaming-availability.p.rapidapi.com/v2/search/title?title=${name}&country=us&type=movie&output_language=en`,
-    options
-  );
+const containner = document.querySelector(".containner");
+const movieName = document.querySelector("#movieNameInput");
+const searchButton = document.querySelector("#search-btn");
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+
+async function fetchMovie(url) {
+  const res = await fetch(url);
   const data = await res.json();
   console.log(data);
-  // console.log(data.result[0].posterPath);
-  let url = data.result[0].posterURLs[500];
-  console.log(url);
-  image.src = url;
+  const movieList = data.results;
+  movieList.forEach((movie) => {
+    let { title, poster_path, vote_average } = movie;
+    console.log(title, poster_path, vote_average, movie);
+
+    if (movie.poster_path) {
+      containner.innerHTML += ` <div class="movie-card">
+    <img src='${
+      IMGPATH + poster_path
+    }' alt="posterImg"    id="image" class='poster' />
+    <div class="details">
+    <div class='info'>
+    <h1>${title}</h1>
+    <span class='${getClassbyRating(
+      vote_average
+    )}' > <i class="fa-solid fa-star"></i>${vote_average.toFixed(1)}</span>
+    </div>
+    </div>
+  </div>`;
+    }
+  });
 }
-fetchMovie("batman");
+function getClassbyRating(rating) {
+  if (rating >= 8) return "green";
+  else if (rating >= 5) return "orange";
+  else return "red";
+}
+
+// fetching default movies
+fetchMovie(APIURL);
+
+// fetching movie on serach term
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  containner.innerHTML = "";
+  const nameValue = movieName.value;
+  fetchMovie(SEARCHAPI + nameValue);
+});
