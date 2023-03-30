91ab2a3ad1ad32f6b1e465db484749e107d98174
Dhairyash Gupta <101561153+Dhairyash-1@users.noreply.github.com>
2023-03-30 16:46:18 +0530

    basic calculator done

---
+++ Calculator/index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const outputScreen = document.querySelector(".screen");
+const numberBtn = document.querySelectorAll(".number-btn");
+const operatorBtn = document.querySelectorAll(".operator-btn");
+const equalBtn = document.querySelector(".equal-btn");
+const allClearBtn = document.querySelector(".all-clear");
+const deleteBtn = document.querySelector(".delete");
+
+// displaying the clicked number on screen
+numberBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let inputValue = e.target.innerText;
+    outputScreen.textContent += inputValue;
+  });
+});
+
+// displaying the clicked operator on screen
+
+operatorBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let inputValue = e.target.innerText;
+    if (outputScreen.textContent.at(-1) == "+") {
+      outputScreen.textContent += "";
+    } else if (outputScreen.textContent.at(-1) == "-") {
+      outputScreen.textContent += "";
+    } else if (outputScreen.textContent.at(-1) == "*") {
+      outputScreen.textContent += "";
+    } else if (outputScreen.textContent.at(-1) == "/") {
+      outputScreen.textContent += "";
+    } else {
+      outputScreen.textContent += inputValue;
+    }
+  });
+});
+
+// displaying the evaluated value on equal button clik
+equalBtn.addEventListener("click", () => {
+  try {
+    let result = eval(outputScreen.textContent);
+    outputScreen.textContent = "";
+    outputScreen.textContent = result;
+  } catch {
+    outputScreen.innerText = "";
+    outputScreen.innerText = "Invaild Input";
+  }
+});
+
+// clearing all values from screen
+allClearBtn.addEventListener("click", () => (outputScreen.textContent = ""));
+
+// clearing lastEntered value from screen
+deleteBtn.addEventListener("click", () => {
+  let string = outputScreen.textContent;
+  let updatedString = string.slice(0, string.length - 1);
+  outputScreen.textContent = updatedString;
+});
